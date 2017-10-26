ed8a2a46a3098adf170e622e9a789b1737bed62b
aaronkeith <aflisik@gmail.com>
2017-10-26 15:55:54 -0700

    Coordinates of buttons can now viewed in console via mouseenter #1

--- public/main.js
+++ public/main.js
@@ -1,24 +1,32 @@
-const $tooltips = document.querySelector('.tooltips')
+const $button = document.querySelectorAll('.button')
+const intViewportHeight = window.innerHeight
+const intViewportWidth = window.innerWidth
 
-$tooltips.forEach($tooltip)
-$tooltip.addEventListener('mouseover', event => {
-  const { top, bottom, left, right } = $tooltip.getBoundingClientRect()
-  if (top > 20) {
-    event.target.classList.remove('bottom', 'left', 'right')
-    event.target.classList.add('top')
-  } else if (right > 120) {
-    event.target.classList.remove('bottom', 'left', 'top')
-    event.target.classList.add('right')
-  } else if (left > 120) {
-    event.target.classList.remove('bottom', 'right', 'top')
-    event.target.classList.add('left')
-  } else if (bottom > 20) {
-    event.target.classList.remove('left', 'right', 'top')
-    event.target.classList.add('bottom')
-  }
+$button.forEach(el => {
+  const xCoord = el.getBoundingClientRect().x
+  const yCoord = el.getBoundingClientRect().y
+  console.log(xCoord)
+  console.log(yCoord)
+  el.addEventListener('mouseenter', event => {
+    const { top, bottom, left, right } = el.getBoundingClientRect()
+     if(xCoord ) {
+      event.target.classList.remove('bottom', 'left', 'right')
+      event.target.classList.add('top')
+      console.log(yCoord)
+    }
+    else if (right > 150 || top < 30) {
+      event.target.classList.remove('bottom', 'left', 'top')
+      event.target.classList.add('right')
+    }
+    else if
+    (left > 150 || top < 30) {
+      event.target.classList.remove('bottom', 'right', 'top')
+      event.target.classList.add('left')
+    }
+    else if
+    (bottom > 20) {
+      event.target.classList.remove('left', 'right', 'top')
+      event.target.classList.add('bottom')
+    }
+  })
 })
-
-console.log(`${top} px from the top of the window`)
-console.log(`${window.innerHeight - bottom}px from the bottom of the window`)
-console.log(`${left}px from the left of the window`)
-console.log(`${window.innerWidth - right} from the right of the window`)
