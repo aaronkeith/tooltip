29dedfd8766e06996079f58ca394478d91eda342
aaronkeith <aflisik@gmail.com>
2017-10-27 13:10:24 -0700

    Removed unnecessary console.log functions. #1

--- public/main.js
+++ public/main.js
@@ -9,24 +9,20 @@ $button.forEach(el => {
      if(yCoord > 40 && xCoord > 120 && (intViewportWidth - xCoord > 255) ) {
       event.target.classList.remove('tooltip-bottom', 'tooltip-left', 'tooltip-right')
       event.target.classList.add('tooltip-top')
-      console.log('top');
     }
     else if ((yCoord > 20) && (intViewportWidth - xCoord) > 250)  {
       event.target.classList.remove('tooltip-bottom', 'tooltip-left', 'tooltip-top')
       event.target.classList.add('tooltip-right')
-      console.log('right');
     }
     else if
     (yCoord > 20 && (intViewportWidth - xCoord) < 250) {
       event.target.classList.remove('tooltip-bottom', 'tooltip-right', 'tooltip-top')
       event.target.classList.add('tooltip-left')
-      console.log('left');
     }
     else if
     (yCoord <= 0 && (intViewportWidth - xCoord) > 50) {
       event.target.classList.remove('tooltip-left', 'tooltip-right', 'tooltip-top')
       event.target.classList.add('tooltip-bottom')
-      console.log('bottom');
     }
   })
 })
